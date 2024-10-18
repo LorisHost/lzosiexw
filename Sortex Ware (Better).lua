bf392c50a10d2fbd718a450cc6cbf3c936961f40
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-10-18 19:00:10 +0300

    Update Sortex Ware (Better).lua

--- Sortex Ware (Better).lua
+++ Sortex Ware (Better).lua
@@ -1,4 +1,3 @@
--- idea Got From Speed Hub X
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/GameList"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
