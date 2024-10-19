61d55e3931cfc17a9090dcf05df73d734109887b
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-10-19 14:01:00 +0300

    Update Sortex Ware (Better).lua

--- Sortex Ware (Better).lua
+++ Sortex Ware (Better).lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/GameList"))()
-for PlaceID, Execute in pairs(Games) do
+for PlaceID, Execute in pairs(PlaceId) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
     end
