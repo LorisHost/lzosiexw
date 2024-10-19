d2de3ee748e4aa1566af0706b35e3768a11b3763
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-10-19 15:19:07 +0300

    Update Sortex Ware (Better).lua

--- Sortex Ware (Better).lua
+++ Sortex Ware (Better).lua
@@ -1,11 +1,2 @@
+-- Stolen Idea Form Speed X Hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/GameList"))()
-local g = getgenv
-local p = g.PlaceId
-
-if p then
-    for _, id in pairs(p) do
-        if id == game.PlaceId then
-            loadstring(game:HttpGet("Execute"))()
-        end
-    end
-end
