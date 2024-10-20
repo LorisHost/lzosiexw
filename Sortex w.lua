b9940804bd2d1a7ca2ce82c0c674f1a61fab6006
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-10-20 11:31:39 +0300

    Update Sortex w.lua

--- Sortex w.lua
+++ Sortex w.lua
@@ -1,6 +1,4 @@
--- Stolen Idea Form Speed X Hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/GameList"))()
-
 for _, id in pairs(PlaceId) do
     if id == game.PlaceId then
         loadstring(game:HttpGet("Execute"))()
