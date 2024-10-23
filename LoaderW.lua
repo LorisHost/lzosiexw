a50efcd3840f2cc568baa0b8ddcdea5ae0b2763a
LorisHost <130068806+LorisHost@users.noreply.github.com>
2024-10-23 13:49:01 +0300

    Update LoaderW.lua

--- LoaderW.lua
+++ LoaderW.lua
@@ -1,6 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LorisHost/lzosiexw/refs/heads/main/GameList"))()
-for _, id in pairs(PlaceId) do
-    if id == game.PlaceId then
-        loadstring(game:HttpGet("Execute"))()
-    end
-end
+Remake?
