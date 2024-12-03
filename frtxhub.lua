35ff4a8eb0867961e68dda2b71707f008dfce56a
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2024-12-03 15:33:15 +0200

    Update frtxhub.lua

--- frtxhub.lua
+++ frtxhub.lua
@@ -1,7 +1,8 @@
---888888  dP"Yb  88""Yb 888888 888888 Yb  dP   88   88 88b 88 88 Yb    dP 888888 88""Yb .dP"Y8    db    88     
---88__   dP   Yb 88__dP   88   88__    YbdP    88   88 88Yb88 88  Yb  dP  88__   88__dP `Ybo."   dPYb   88     
---88""   Yb   dP 88"Yb    88   88""    dPYb    Y8   8P 88 Y88 88   YbdP   88""   88"Yb  o.`Y8b  dP__Yb  88  .o 
---88      YbodP  88  Yb   88   888888 dP  Yb   `YbodP' 88  Y8 88    YP    888888 88  Yb 8bodP' dP""""Yb 88ood8 
+--.dP"Y8 Yb        dP    db    88""Yb   88   88 88b 88 88 Yb    dP 888888 88""Yb .dP"Y8    db    88     
+--`Ybo."  Yb  db  dP    dPYb   88__dP   88   88 88Yb88 88  Yb  dP  88__   88__dP `Ybo."   dPYb   88     
+--o.`Y8b   YbdPYbdP    dP__Yb  88"""    Y8   8P 88 Y88 88   YbdP   88""   88"Yb  o.`Y8b  dP__Yb  88  .o 
+--8bodP'    YP  YP    dP""""Yb 88       `YbodP' 88  Y8 88    YP    888888 88  Yb 8bodP' dP""""Yb 88ood8 
+
               --Credits : Tencent 
               --Licensed : API Sortex.  Thanks Yall For Support This Script
               --Support : Vega x , Delta x , Evon , Arceus x , Code x and others 
