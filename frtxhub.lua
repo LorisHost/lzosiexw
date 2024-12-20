6b8e7daaf8783ef92121cbae0f6113f2a5643bb5
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2024-12-20 14:00:55 +0200

    Update frtxhub.lua

--- frtxhub.lua
+++ frtxhub.lua
@@ -10,21 +10,13 @@
               --Obsfucator : Moonsec V3
               --WARNING : im not responsible for this script using or Banned Account (Sometimes Gui can Be Dectected or Patched will switch to another)
               --This Script protect to not being Patches
-local a = {104, 116, 116, 112, 115, 58, 47, 47}
-local b = {112, 97, 115, 116, 101, 46, 103, 103, 47}
-local c = {112, 47, 115, 111, 114, 120, 46, 47}
-local d = {54, 48, 49, 54, 53, 99, 98, 55, 51, 51, 54, 48, 52, 51, 99, 49, 98, 49, 51, 98, 101, 99, 52, 99, 55, 49, 53, 97, 51, 57, 99, 55, 47}
-local e = {102, 105, 108, 101, 115, 47}
-local f = {51, 57, 56, 99, 99, 57, 50, 102, 49, 50, 54, 99, 52, 54, 50, 100, 57, 100, 51, 102, 99, 56, 54, 56, 52, 50, 100, 101, 98, 52, 49, 101, 47}
-local g = {114, 97, 119}
+local a = {"h", "#", 116, "$", "t", "&", 112, "*", "s", ":", "/", "/", 47, "@"}
+local b = {112, "a", 115, "^", "t", "!", 101, ".", "g", 103, "/", "g", "%"}
+local c = {"p", "/", "s", "&", "o", "@", "r", "x", ".", "/", 47, "#"}
+local d = {"6", "0", 49, "$", "6", 53, "c", "&", 98, 55, 51, 51, "6", 48, "4", "3", "c", "1", "b", "1", "@", 51, "b", 101, "c", 52, "c", 55, "1", "5", "a", 51, "9", "c", 55, "/", "$"}
+local e = {"f", "@", "i", "&", "l", "*", "e", "s", "/", "#"}
+local f = {"3", "9", 56, "*", "c", "c", 57, "@", 50, "f", "#", "1", "2", 54, "c", 52, "&", "6", 50, "d", 57, "d", 51, "*", "f", "c", 56, 54, "8", "4", "2", "&", "d", "e", "b", "4", 49, "e", "/", "#"}
+local g = {"r", "&", "a", "*", "w", "#", 114}
 
-local function decode(t)
-    local s = ""
-    for _, v in ipairs(t) do
-        s = s .. string.char(v)
-    end
-    return s
-end
-
-local url = decode(a) .. decode(b) .. decode(c) .. decode(d) .. decode(e) .. decode(f) .. decode(g)
-loadstring(game:HttpGet(url))()
+local function decode(t) return table.concat({unpack(t)}, ""):gsub("%d+", function(c) return string.char(tonumber(c)) end) end
+loadstring(game:HttpGet(decode{a,b,c,d,e,f,g}))()
