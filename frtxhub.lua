a2b144255c265d50e3c041c6be139098c85e6cb8
SORTEX (TENCENT) <130068806+LorisHost@users.noreply.github.com>
2024-12-20 14:08:16 +0200

    Update frtxhub.lua

--- frtxhub.lua
+++ frtxhub.lua
@@ -1,9 +1,6 @@
---.dP"Y8 Yb        dP    db    88""Yb   88   88 88b 88 88 Yb    dP 888888 88""Yb .dP"Y8    db    88     
---`Ybo."  Yb  db  dP    dPYb   88__dP   88   88 88Yb88 88  Yb  dP  88__   88__dP `Ybo."   dPYb   88     
---o.`Y8b   YbdPYbdP    dP__Yb  88"""    Y8   8P 88 Y88 88   YbdP   88""   88"Yb  o.`Y8b  dP__Yb  88  .o 
---8bodP'    YP  YP    dP""""Yb 88       `YbodP' 88  Y8 88    YP    888888 88  Yb 8bodP' dP""""Yb 88ood8 
+                                                                                                                                                
 
-              --Credits : Tencent 
+               --Credits : Tencent 
               --Licensed : API Sortex.  Thanks Yall For Support This Script
               --Support : Vega x , Delta x , Evon , Arceus x , Code x and others 
               --Support (PC) : Synpase Z , Wave , Rebel , ignite . (Some Executors Not supported Moonsec)
